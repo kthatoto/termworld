8806a782df5c227d6ed190726979a7c3c3ccb7db
kthatoto <kthatoto@gmail.com>
2020-11-24 03:24:46 +0900

    Use cobra directory pattern

--- main.go
+++ main.go
@@ -1,53 +1,9 @@
 package main
 
 import (
-	"net/url"
-	"net/http"
-	"fmt"
-	"os"
-	"bufio"
-	"strings"
-	"ioutil"
-
-	"github.com/spf13/cobra"
-	"github.com/gorilla/websocket"
+	"github.com/kthatoto/termworld/cmd"
 )
 
 func main() {
-	u := url.URL{Scheme: "ws", Host: "localhost:8080", Path: "/gaming"}
-	httpHeader := http.Header{}
-	bytes, err := ioutil.ReadFile("./token")
-	if err != nil {
-		panic(err)
-	}
-	httpHeader.Set("X-Termworld-Token", string(bytes))
-	conn, _, err := websocket.DefaultDialer.Dial(u.String(), httpHeader)
-	if err != nil {
-		panic(err)
-	}
-	defer conn.Close()
-
-	go func() {
-		for {
-			_, message, err := conn.ReadMessage()
-			if err != nil {
-				fmt.Println("read:", err)
-				return
-			}
-			for _, line := range strings.Split(string(message), "\n") {
-				fmt.Printf("read: %s\n", line)
-			}
-		}
-	}()
-
-	stdin := bufio.NewScanner(os.Stdin)
-	for stdin.Scan() {
-		text := stdin.Text()
-		err := conn.WriteMessage(websocket.TextMessage, []byte(text))
-		fmt.Printf("wrote [%s]\n", text)
-		if err != nil {
-			fmt.Println("write:", err)
-			return
-		}
-	}
+	cmd.Run()
 }
