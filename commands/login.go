45e2eb128c53eec58d574b32ab6db8554342a0ea
kthatoto <kthatoto@gmail.com>
2020-11-24 06:49:50 +0900

    Save token to config

--- commands/login.go
+++ commands/login.go
@@ -8,8 +8,10 @@ import (
 	"bytes"
 	"net/http"
 	"errors"
+	"io/ioutil"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func init() {
@@ -32,9 +34,9 @@ var loginCommand = &cobra.Command{
 		type RequestBody struct {
 			Email string `json:"email"`
 		}
-		json, _ := json.Marshal(RequestBody{email})
+		param, _ := json.Marshal(RequestBody{email})
 		u := "http://localhost:8080/login/new"
-		resp, err := http.Post(u, "application/json", bytes.NewBuffer(json))
+		resp, err := http.Post(u, "application/json", bytes.NewBuffer(param))
 		if err != nil {
 			return err
 		}
@@ -44,15 +46,28 @@ var loginCommand = &cobra.Command{
 		fmt.Printf("Sent mail to %s\nPlease check and click link on the mail to verify\n", email)
 
 		u = "http://localhost:8080/login"
-		var token string
 		for {
-			resp, err := http.Post(u, "application/json", bytes.NewBuffer(json))
+			resp, err := http.Post(u, "application/json", bytes.NewBuffer(param))
 			if err != nil {
 				return err
 			}
+			defer resp.Body.Close()
 			if resp.StatusCode == 408 {
 				continue
 			} else if resp.StatusCode == 200 {
+				bytes, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+					return err
+				}
+				type ResponseBody struct {
+					Token string `json:"token"`
+				}
+				var responseBody ResponseBody
+				if err := json.Unmarshal(bytes, &responseBody); err != nil {
+					return err
+				}
+				viper.Set("token", responseBody.Token)
+				viper.WriteConfig()
 				break
 			} else {
 				return errors.New("Request failed")
