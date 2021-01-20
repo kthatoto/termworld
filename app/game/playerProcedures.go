235fcf9fa86200d70e0675dc2fe89bb39199f053
kthatoto <kthatoto@gmail.com>
2021-01-21 03:59:47 +0900

    Send request id for websocket messaging

--- app/game/playerProcedures.go
+++ app/game/playerProcedures.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 
 	"github.com/gorilla/websocket"
+
+	"github.com/kthatoto/termworld/utils"
 )
 
 type PlayerProcedures int
@@ -13,7 +15,12 @@ type PlayerProcedureArgs struct {
 }
 
 func (p *PlayerProcedures) Start(args PlayerProcedureArgs, result *bool) error {
-	message := fmt.Sprintf("{\"playerName\":\"%s\",\"command\":\"start\",\"options\":null}", args.PlayerName)
+	requestId := utils.RandomString(12)
+	message := fmt.Sprintf(
+		"{\"playerName\":\"%s\",\"command\":\"start\",\"options\":null,\"requestId\":\"%s\"}",
+		args.PlayerName,
+		requestId,
+	)
 	WSConn.WriteMessage(websocket.TextMessage, []byte(message))
 	*result = true
 	return nil
---
+++ utils/randomString.go
@@ -0,0 +1,15 @@
+package utils
+
+import (
+	"math/rand"
+)
+
+func RandomString(length int) string {
+	var letter = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = letter[rand.Intn(len(letter))]
+	}
+	return string(b)
+}
