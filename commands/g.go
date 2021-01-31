54f6d05156b4e89fd312567fd22a3acced91d5e3
kthatoto <kthatoto@gmail.com>
2021-02-01 01:09:21 +0900

    Add g command

---
+++ commands/g.go
@@ -0,0 +1,56 @@
+package commands
+
+import (
+	// "fmt"
+	// "strings"
+	// "net/rpc"
+
+	"github.com/spf13/cobra"
+)
+
+type PlayerProcedureArgs struct {
+	PlayerName string
+	Options    []string
+}
+
+func init() {
+	rootCommand.AddCommand(playerCommand)
+}
+
+var playerCommand = &cobra.Command{
+	Use: "player",
+	Short: "Player command",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cmd.Usage()
+		return nil
+		// if (len(args) == 0) {
+		// 	cmd.Usage()
+		// 	return nil
+		// }
+        //
+		// playerName := args[0]
+		// command := args[1]
+		// options := args[2:]
+		// client, err := rpc.DialHTTP("tcp", "localhost:8128")
+		// if err != nil {
+		// 	return err
+		// }
+		// proceduresCommand := fmt.Sprintf("PlayerProcedures.%s", strings.Title(command))
+        //
+		// playerProcedureArgs := PlayerProcedureArgs{ playerName, options }
+		// var response interface{}
+		// response = nil
+		// err = client.Call(proceduresCommand, playerProcedureArgs, &response)
+        //
+		// if err != nil {
+		// 	if strings.Contains(err.Error(), "can't find method") {
+		// 		fmt.Printf("error: can't find method [%s]\n", command)
+		// 		return nil
+		// 	}
+		// 	fmt.Printf("error: %s\n", err)
+		// 	return nil
+		// }
+		// fmt.Println(response)
+		// return nil
+	},
+}
--- commands/player.go
+++ commands/player.go
@@ -1,10 +1,6 @@
 package commands
 
 import (
-	// "fmt"
-	// "strings"
-	// "net/rpc"
-
 	"github.com/spf13/cobra"
 )
 
@@ -23,34 +19,5 @@ var playerCommand = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		cmd.Usage()
 		return nil
-		// if (len(args) == 0) {
-		// 	cmd.Usage()
-		// 	return nil
-		// }
-        //
-		// playerName := args[0]
-		// command := args[1]
-		// options := args[2:]
-		// client, err := rpc.DialHTTP("tcp", "localhost:8128")
-		// if err != nil {
-		// 	return err
-		// }
-		// proceduresCommand := fmt.Sprintf("PlayerProcedures.%s", strings.Title(command))
-        //
-		// playerProcedureArgs := PlayerProcedureArgs{ playerName, options }
-		// var response interface{}
-		// response = nil
-		// err = client.Call(proceduresCommand, playerProcedureArgs, &response)
-        //
-		// if err != nil {
-		// 	if strings.Contains(err.Error(), "can't find method") {
-		// 		fmt.Printf("error: can't find method [%s]\n", command)
-		// 		return nil
-		// 	}
-		// 	fmt.Printf("error: %s\n", err)
-		// 	return nil
-		// }
-		// fmt.Println(response)
-		// return nil
 	},
 }
